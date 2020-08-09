b426cb886de5c3432359a1dd1898a720022d51e8
COLDTURNIP <coldturnip@gmail.com>
2020-08-09 23:50:54 +0800

    Correct problem metadata

--- rust/src/p1.rs
+++ rust/src/p1.rs
@@ -1,5 +1,5 @@
 /*
-Problem 18. 4Sum
+Problem 1. Two Sum
 =======================================
 
 https://leetcode.com/problems/two-sum/
@@ -16,11 +16,11 @@ impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut seen = HashMap::with_capacity(nums.len());
 
-        for (ia, a) in nums.iter().enumerate() {
-            let b = target - a;
-            match seen.get(&b) {
-                Some(&ib) => return vec![ia as i32, ib as i32],
-                None => seen.insert(a, ia),
+        for (cur_i, cur_v) in nums.iter().enumerate() {
+            let rest = target - cur_v;
+            match seen.get(&rest) {
+                Some(&seen_i) => return vec![seen_i as i32, cur_i as i32],
+                None => seen.insert(cur_v, cur_i),
             };
         }
         unreachable!();
